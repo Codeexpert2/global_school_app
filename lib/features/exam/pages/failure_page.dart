ce49000f73b897e079b6c0f0be510cb817857213
Ahmad Aldali <Dali@gmail.com>
2025-01-21 11:27:50 +0300

    edit ui

--- lib/features/exam/pages/failure_page.dart
+++ lib/features/exam/pages/failure_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:lottie/lottie.dart';
 
 class FailureQuizPage extends StatelessWidget {
@@ -10,10 +9,65 @@ class FailureQuizPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Color(0xFFB24592), Color(0xFF8E44AD)],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+          ),
           Center(
-            child: Lottie.asset(
-              'assets/animation/failure.json',
-              fit: BoxFit.cover,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset(
+                  'assets/animation/failure.json',
+                  // width: 100,
+                  // height: 100,
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'لا بأس!',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(0, 3),
+                        blurRadius: 5,
+                        color: Colors.black26,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'لم توفق هذه المرة، لكن الفرصة ما زالت قائمة.\nحاول مرة أخرى ونحن نؤمن بك! 💪',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    color: Colors.white70,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'عودة إلى الصفحة الرئيسية',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white70,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
--- lib/features/exam/pages/success_page.dart
+++ lib/features/exam/pages/success_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:lottie/lottie.dart';
 
 class SuccessQuizPage extends StatelessWidget {
@@ -11,9 +10,52 @@ class SuccessQuizPage extends StatelessWidget {
       body: Stack(
         children: [
           Center(
-            child: Lottie.asset(
-              'assets/animation/Animation - 1737409561651.json',
-              fit: BoxFit.cover,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset(
+                  'assets/animation/Animation - 1737409561651.json',
+                  width: 250,
+                  height: 250,
+                  fit: BoxFit.cover,
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'تهانينا!',
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(0, 3),
+                        blurRadius: 5,
+                        color: Colors.black26,
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'لقد نجحت في الامتحان بنجاح كبير 🎉',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text(
+                    'عودة إلى الصفحة الرئيسية',
+                    style: TextStyle(
+                      fontSize: 16,
+                      decoration: TextDecoration.underline,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
