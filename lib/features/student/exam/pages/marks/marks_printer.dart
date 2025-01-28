555b000ea5ba405f0321b1abe3071b4c199df02a
Ahmad Aldali <Dali@gmail.com>
2025-01-28 11:21:54 +0300

    fix print marks

--- lib/features/student/exam/pages/marks/marks_printer.dart
+++ lib/features/student/exam/pages/marks/marks_printer.dart
@@ -21,64 +21,60 @@ class PdfPrinterPage {
 
     pdf.addPage(
       pw.Page(
-        build: (pw.Context context) => pw.Column(
-          crossAxisAlignment: pw.CrossAxisAlignment.end, // المحاذاة للنصوص من اليمين
-          children: [
-            pw.Text(
-              'الجلاء المدرسي',
-              textDirection: pw.TextDirection.rtl, // اتجاه النص من اليمين لليسار
-              style: pw.TextStyle(
-                fontSize: 24,
-                fontWeight: pw.FontWeight.bold,
-                font: cairoFont,
+        build: (pw.Context context) => pw.Directionality(
+          textDirection: pw.TextDirection.rtl,
+          child: pw.Column(
+            crossAxisAlignment: pw.CrossAxisAlignment.start,
+            children: [
+              pw.Text(
+                'الجلاء المدرسي',
+                style: pw.TextStyle(
+                  fontSize: 24,
+                  fontWeight: pw.FontWeight.bold,
+                  font: cairoFont,
+                ),
               ),
-            ),
-            pw.SizedBox(height: 20),
-            pw.Text(
-              'اسم الطالب: $studentName',
-              textDirection: pw.TextDirection.rtl,
-              style: pw.TextStyle(font: cairoFont),
-            ),
-            pw.Text(
-              'اسم الأب: $fatherName',
-              textDirection: pw.TextDirection.rtl,
-              style: pw.TextStyle(font: cairoFont),
-            ),
-            pw.Text(
-              'اسم الأم: $motherName',
-              textDirection: pw.TextDirection.rtl,
-              style: pw.TextStyle(font: cairoFont),
-            ),
-            pw.Text(
-              'الصف: $studentClass',
-              textDirection: pw.TextDirection.rtl,
-              style: pw.TextStyle(font: cairoFont),
-            ),
-            pw.SizedBox(height: 20),
-            pw.Table.fromTextArray(
-              headers: ['المادة', 'العلامة الكاملة', 'علامة الطالب'],
-              data: subjects
-                  .map((subject) => [
-                        subject['name'],
-                        subject['fullMark'],
-                        subject['studentMark'],
-                      ])
-                  .toList(),
-              cellAlignment: pw.Alignment.centerRight, 
-              cellStyle: pw.TextStyle(
-                font: cairoFont,
-                // textDirection: pw.TextDirection.rtl,
+              pw.SizedBox(height: 20),
+              pw.Text(
+                'اسم الطالب: $studentName',
+                style: pw.TextStyle(font: cairoFont),
               ),
-              headerStyle: pw.TextStyle(
-                font: cairoFont,
-                fontWeight: pw.FontWeight.bold,
-                // textDirection: pw.TextDirection.rtl,
+              pw.Text(
+                'اسم الأب: $fatherName',
+                style: pw.TextStyle(font: cairoFont),
               ),
-              headerDecoration: const pw.BoxDecoration(
-                color: PdfColors.grey300,
+              pw.Text(
+                'اسم الأم: $motherName',
+                style: pw.TextStyle(font: cairoFont),
               ),
-            ),
-          ],
+              pw.Text(
+                'الصف: $studentClass',
+                style: pw.TextStyle(font: cairoFont),
+              ),
+              pw.SizedBox(height: 20),
+              pw.Table.fromTextArray(
+                headers: ['المادة', 'العلامة الكاملة', 'علامة الطالب'],
+                data: subjects
+                    .map((subject) => [
+                          subject['name'],
+                          subject['fullMark'],
+                          subject['studentMark'],
+                        ])
+                    .toList(),
+                cellAlignment: pw.Alignment.centerRight,
+                cellStyle: pw.TextStyle(
+                  font: cairoFont,
+                ),
+                headerStyle: pw.TextStyle(
+                  font: cairoFont,
+                  fontWeight: pw.FontWeight.bold,
+                ),
+                headerDecoration: const pw.BoxDecoration(
+                  color: PdfColors.grey300,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
