120d1094f3d07ac50c35a7c83e2117e50180a8c9
Essa Sabbagh <50638748+essasabbagh@users.noreply.github.com>
2025-02-13 10:50:07 +0300

    Redesign Splash screen

--- lib/features/shared/splash/splash_screen.dart
+++ lib/features/shared/splash/splash_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:global_school/components/loading/loading_widget.dart';
+import 'package:global_school/core/constants/images.dart';
+import 'package:global_school/core/extensions/context_ext.dart';
+
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
@@ -7,8 +11,26 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset(
-          'assets/images/madares_icon.png',
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Spacer(
+              flex: 2,
+            ),
+            Image.asset(
+              AppImages.imagesMadaresIcon,
+            ),
+            const SizedBox(height: 24),
+            const LoadingWidget(),
+            const Spacer(),
+            const Text('Devloped By '),
+            Image.asset(
+              AppImages.imagesCodeExpert,
+              width: context.width / 3,
+            ),
+            const SizedBox(height: kToolbarHeight),
+          ],
         ),
       ),
     );
