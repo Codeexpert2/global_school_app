3c5c75a8d3db81ed2b60ec578a414638e3b11e71
Ahmad Aldali <Dali@gmail.com>
2025-02-15 11:26:05 +0300

    edit ui certoficate

--- lib/features/student/certificate/page/certificate_details_page.dart
+++ lib/features/student/certificate/page/certificate_details_page.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:global_school/core/locale/generated/l10n.dart';
+
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import 'package:global_school/core/locale/generated/l10n.dart';
+import 'package:global_school/core/themes/app_colors.dart';
+import 'package:global_school/core/themes/app_gradients.dart';
+import 'package:global_school/core/utils/snackbars.dart';
 import 'package:global_school/features/student/certificate/provider/certificate_provider.dart';
 
 class CertificateDetailsPage extends ConsumerWidget {
   const CertificateDetailsPage({super.key, required this.id});
   final String id;
+
+  String _getMotivationalMessage(double? gpa) {
+    if (gpa == null) return 'لا يوجد بيانات عن المعدل التراكمي.';
+    if (gpa >= 90) {
+      return 'معدل تراكمي ممتاز! أنت مثال للتفوق والتميز. استمر في تحقيق النجاحات!';
+    } else if (gpa >= 70) {
+      return 'معدل تراكمي جيد! أنت على الطريق الصحيح، واصل العمل بجد لتحقيق المزيد.';
+    } else {
+      return 'معدل تراكمي يحتاج إلى تحسين. لا تيأس، كل بداية صعبة، ولكن المثابرة ستوصلك إلى القمة.';
+    }
+  }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -13,33 +29,86 @@ class CertificateDetailsPage extends ConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title:  Text(S.of(context).certificateDetails),
+        title: Text(
+          S.of(context).certificateDetails,
+        ),
+        centerTitle: true,
       ),
       body: certificateData.when(
         data: (certificate) {
-          return Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Card(
-                  margin: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                    title: Text('المعدل التراكمي: ${certificate.gpa}'),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('الحالة: ${certificate.status ?? "غير معروف"}'),
-                      ],
+          return Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.9,
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                gradient: AppGradient.linearGradient,
+                borderRadius: BorderRadius.circular(25),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    blurRadius: 12,
+                    spreadRadius: 3,
+                  ),
+                ],
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(Icons.school, size: 60, color: Colors.white),
+                  const SizedBox(height: 15),
+                  const Text(
+                    'Certificate of Achievement',
+                  ),
+                  const SizedBox(height: 15),
+                  const Text(
+                    'Ahmad Aldali',
+                    // certificate.user?.name ?? 'اسم الطالب غير متوفر',
+                  ),
+                  const SizedBox(height: 10),
+                  const Text(
+                    'Number: 123456',
+                    // 'الرقم التسلسلي: ${certificate.user?.serialNumber ?? "غير متوفر"}',
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    "GPA: ${certificate.gpa ?? "غير متوفر"}",
+                  ),
+                  const SizedBox(height: 20),
+                  Text(
+                    _getMotivationalMessage(
+                        double.tryParse(certificate.gpa ?? '0')),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 20),
+                  if (certificate.createdAt != null)
+                    Text(
+                      'تاريخ الإنشاء: ${certificate.createdAt}',
+                    ),
+                  const SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    onPressed: () {
+                      showSuccessSnackbar('تم تحميل الشهادة بنجاح.');
+                    },
+                    icon: const Icon(Icons.download, color: AppColors.green2),
+                    label: const Text(
+                      'تحميل الشهادة',
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 12, horizontal: 20),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           );
         },
         loading: () => const Center(child: CircularProgressIndicator()),
-        error: (error, stack) => Center(child: Text('حدث خطأ: $error')),
+        error: (error, stack) => Center(
+            child: Text('حدث خطأ: $error',
+                style: const TextStyle(color: Colors.red))),
       ),
     );
   }
--- lib/features/student/certificate/page/certificates_page.dart
+++ lib/features/student/certificate/page/certificates_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
 import 'package:global_school/components/form/debounced_search.dart';
 import 'package:global_school/core/locale/generated/l10n.dart';
 import 'package:global_school/core/pagination/paginated_list_widget.dart';
 import 'package:global_school/core/router/app_routes.dart';
 import 'package:global_school/features/student/certificate/model/certificate_model.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:global_school/features/student/certificate/provider/certificate_provider.dart';
 
 class CertificatesPage extends ConsumerWidget {
@@ -13,43 +15,88 @@ class CertificatesPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-        appBar: AppBar(
-          title:  Text(S.of(context).certificates),
-          bottom: PreferredSize(
-            preferredSize: const Size.fromHeight(kToolbarHeight),
-            child: DebouncedSearch(
-              onDebounceChange: (value) {
-                // Update search query
-                ref.read(certificateSearchProvider.notifier).state = value;
-                // Refresh the list
-                ref.read(certificateListProvider.notifier).refresh();
-              },
-            ),
+      appBar: AppBar(
+        title: Text(S.of(context).certificates),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(kToolbarHeight),
+          child: DebouncedSearch(
+            onDebounceChange: (value) {
+              // Update search query
+              ref.read(certificateSearchProvider.notifier).state = value;
+              // Refresh the list
+              ref.read(certificateListProvider.notifier).refresh();
+            },
           ),
         ),
-        body: PaginatedListWidget<Certificate>(
-          provider: certificateListProvider,
-          itemBuilder: (context, certificate) {
-            return Card(
-              margin: const EdgeInsets.all(8.0),
-              child: ListTile(
-                title:
-                    Text('المعدل التراكمي: ${certificate.gpa ?? "غير متوفر"}'),
-                subtitle: Text('الحالة: ${certificate.status ?? "غير معروف"}'),
-                trailing: IconButton(
-                  icon: const Icon(Icons.visibility),
-                  onPressed: () {
-                    context.pushNamed(
-                      AppRoutes.studentCertificateDetails.name,
-                      pathParameters: {
-                        'id': certificate.id.toString(),
+      ),
+      body: PaginatedListWidget<Certificate>(
+        provider: certificateListProvider,
+        itemBuilder: (context, certificate) {
+          return Card(
+            margin: const EdgeInsets.all(8.0),
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 8),
+                  Text(
+                    "GPA : ${certificate.gpa ?? "غير متوفر"}",
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.black87,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+
+                  // حالة الشهادة
+                  Row(
+                    children: [
+                      const Icon(Icons.info, size: 16, color: Colors.orange),
+                      const SizedBox(width: 4),
+                      Text(
+                        "Status: ${certificate.status ?? "غير معروف"}",
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  Align(
+                    alignment: Alignment.centerRight,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        context.pushNamed(
+                          AppRoutes.studentCertificateDetails.name,
+                          pathParameters: {
+                            'id': certificate.id.toString(),
+                          },
+                        );
                       },
-                    );
-                  },
-                ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                      child: const Text(
+                        'Show Certificate',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            );
-          },
-        ));
+            ),
+          );
+        },
+      ),
+    );
   }
 }
